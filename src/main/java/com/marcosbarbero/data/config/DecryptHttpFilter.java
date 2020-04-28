5b71bfc161604c542965e72eadf8ed956464d7bb
Marcos Barbero <marcos.hgb@gmail.com>
2020-04-28 16:29:56 +0200

    Change filter type

--- src/main/java/com/marcosbarbero/data/config/DecryptHttpFilter.java
+++ src/main/java/com/marcosbarbero/data/config/DecryptHttpFilter.java
@@ -2,25 +2,31 @@ package com.marcosbarbero.data.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class DecryptHttpFilter extends HttpFilter {
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class DecryptHttpFilter extends OncePerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(DecryptHttpFilter.class);
+
+    private static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
 
     private static final String PARAM_NAME = "customerId";
 
     @Override
-    public void doFilter(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws IOException, ServletException {
-        MutableHttpServletRequest mutableRequest = new MutableHttpServletRequest(httpServletRequest);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        MutableHttpServletRequest mutableRequest = new MutableHttpServletRequest(request);
 
         logger.info(mutableRequest.getRequestURI());
         logParamValue(mutableRequest);
@@ -30,11 +36,15 @@ public class DecryptHttpFilter extends HttpFilter {
 
         logParamValue(mutableRequest);
 
-        filterChain.doFilter(mutableRequest, httpServletResponse);
+        filterChain.doFilter(mutableRequest, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return !PATH_MATCHER.match("/*/search/*", request.getRequestURI().toLowerCase());
     }
 
     private void logParamValue(MutableHttpServletRequest request) {
         logger.info("{}={}", PARAM_NAME, request.getParameter(PARAM_NAME));
     }
-
 }
